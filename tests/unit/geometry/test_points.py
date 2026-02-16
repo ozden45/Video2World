cdcc8fb92402d59a0d935d5705bda2966fe44f0f
ozden45 <ozdenozel@outlook.com>
2026-02-16 16:25:06 +0300

    -

--- tests/unit/geometry/test_points.py
+++ tests/unit/geometry/test_points.py
@@ -9,16 +9,7 @@ class TestPoints:
     def test_add_point(self, p1, p2):
         pts = Points()
         pts += p1
-        print("==============================================")
-        print(f"p1.coords.shape: {p1.coords.shape}")
-        print(f"p1.covariance.shape: {p1.covariance.shape}")
-        print(f"p1.color.shape: {p1.color.shape}")
-        print(f"p1.alpha.shape: {p1.alpha.shape}")
-        print("==============================================")
-        print(f"pts.coords.shape: {pts.coords.shape}")
-        print(f"pts.covariance.shape: {pts.covariances.shape}")
-        print(f"pts.color.shape: {pts.colors.shape}")
-        print(f"pts.alpha.shape: {pts.alphas.shape}")
+        
         assert pts.coords.shape == (1, 3)
         assert pts.covariances.shape == (1, 3, 3)
         assert pts.colors.shape == (1, 3)
@@ -47,8 +38,6 @@ class TestPoints:
         assert _pts.colors.shape == (4, 3)
         assert _pts.alphas.shape[0] == 4
         
-        
-
 
 class TestPointCloud:
     pass
